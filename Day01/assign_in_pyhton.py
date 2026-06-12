395f55aebdca44b7d85be048f5fa42160c2c108c
bon-can1 <arunboban66@gmail.com>
2026-06-12 20:16:56 +0530

    Day1: Learned variables and input

---
+++ Day01/assign_in_pyhton.py
@@ -0,0 +1,15 @@
+# we are using variables to store data in python and assign a value to a 
+#       variable and print the value of the variable
+
+# types of variables in python
+# string
+name = "Arun"
+# integer
+age = 20
+# float
+height = 5.5
+# boolean
+is_student = True
+print(name, age, height, is_student)
+
+# these are the basic variables in python
