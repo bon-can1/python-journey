61b457556a2e45761a6ded24033b283562385054
bon-can1 <arunboban66@gmail.com>
2026-06-16 16:29:35 +0530

    message

---
+++ Day03/operators.py
@@ -0,0 +1,8 @@
+# operators (and(&), or(|)) used to check if two or more
+#            conditional statement is true
+
+age = int(input("enter age for edgible: "))
+if age <= 40 and age >=18:
+    print("edgblie")
+else:
+    print("not edgblie")
