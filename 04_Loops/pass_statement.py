c5be4d75e8f34ba07d7955a8f5a457abef0b6542
bon-can1 <arunboban66@gmail.com>
2026-06-20 19:23:43 +0530

    pass complete with litte clarification

--- 04_Loops/pass_statement.py
+++ 04_Loops/pass_statement.py
@@ -1,1 +1,29 @@
 # Placeholder for future code.
+# Python requires some code inside loops, functions, classes, and conditions. 
+#           If you haven't written the code yet, use pass
+
+for i in range(5):
+
+    if i == 3:
+        pass # here just like contiue the step will skip, but for me i need to some thing while i reach this step in future
+
+    print(i)
+
+# few clarification ->  pass: do nothing
+#                   ->  continue: skips current iteration
+#                   ->  break: Stops loop
+
+####################################################################
+
+choice = input("Choose option: ")
+
+if choice == "1":
+    pass
+
+elif choice == "2":
+    pass
+
+elif choice == "3":
+    print("Exit")
+
+
